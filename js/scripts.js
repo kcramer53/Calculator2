94641c84f81bb2deeff02b324401dae58b294c55
Student <example@example.com>
2019-06-03 14:11:07 -0700

    Add initial commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var substract = function(number1, number2) {
+  return number1 + number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter an number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(multiply(number1, number2));
